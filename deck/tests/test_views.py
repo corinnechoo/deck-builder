a870f84cd2ba6310d73ee1f62c023ad735e62f57
corinne <corinne.choo@dentsuaegis.com>
2019-11-18 01:34:00 +0800

    Add tests for endpoint

--- deck/tests/test_views.py
+++ deck/tests/test_views.py
@@ -3,14 +3,17 @@ from django.test import TestCase, RequestFactory
 from ddt import ddt, data
 
 from ..views import index
+import json
 
 
 @ddt
 class SimpleTest(TestCase):
 
+    fixtures = ['test_data.json']
+
     def setUp(self):
         self.factory = RequestFactory()
-    
+
     def format_input(self, playerClass):
         return {
             "playerClass": playerClass
@@ -22,4 +25,27 @@ class SimpleTest(TestCase):
         response = self.client.post("", p, content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
+    @data('Druid', 'Mage')
+    # there are 27 neutral cards, 1 mage, 4 druid
+    def test_details_success(self, value):
+        """  
 
+        
+        """
+        p = self.format_input(value)
+        response = self.client.post("", p, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+        response_json = json.loads(response.content)
+        self.assertTrue(len(response_json) <= 30)
+
+        name_store = dict()
+
+        for row in response_json:
+            name = row['name']
+            if name in name_store:
+                name_store[name] += 1
+            else:
+                name_store[name] = 1
+        for _, val in name_store.items():
+            self.assertTrue(val <= 2)
